6b64f31e37e21e42b64f0e346dbd032cac67a5dd
pandabrand <pandabrand@gmail.com>
2015-07-19 01:17:38 +0000

    author directory is read and authors are loaded on start

--- comic_app.rb
+++ comic_app.rb
@@ -4,10 +4,19 @@ require 'sinatra/base'
 
 class ComicApp < Sinatra::Base
   set :static, true
-  set :public, File.dirname(__FILE__) + '/static'
+  set :public_dir, File.dirname(__FILE__) + '/static'
+  if !Dir.exist?(File.join(settings.public_dir, 'authors'))
+    Dir.mkdir(File.join(app.settings.public_dir, 'authors'))
+  end
+  set :authors, Proc.new { Dir.entries(File.join(settings.public_dir, 'authors')).select {|entry| File.directory? File.join(settings.public_dir, 'authors',entry) and !(entry =='.' || entry == '..') } }
+
+  def author_name(index)
+    name_array = settings.authors[index].split('_')
+    return name_array[1].capitalize + ' ' + name_array[0].capitalize
+  end
   
   get '/' do
-    "Hello World"
+    "John Biggers"
   end
   
   get '/hi' do
@@ -19,8 +28,9 @@ class ComicApp < Sinatra::Base
     redirect "/" + author + "/1"
   end
   
-  get ':author/:page' do
-    author = params['author']
-    author
+  get '/:author/:page' do
+    @author = params['author']
+    @page = params['page']
+    @author + ' page ' + @page
   end
 end
--- comictest.rb
+++ comictest.rb
@@ -3,14 +3,15 @@
 require './comic_app.rb'
 require 'rspec'
 require 'rack/test'
+require 'spec_helper'
 
 describe 'Comic App' do
   include Rack::Test::Methods
   
   def app
-    ComicApp.new
+    ComicApp.new!
   end
-  
+
   it "says Test" do
     get '/hi'
     expect(last_response).to be_ok
@@ -21,4 +22,46 @@ describe 'Comic App' do
     get '/fred'
     expect(last_response.location).to eq('http://example.org/fred/1')
   end
+  
+  it "contains first author" do
+    get '/'
+    expect(last_response.body).to include('John Biggers')
+  end
+  
+  it 'should have authors directory' do
+    expect(Dir.entries(app.settings.public_dir)).to include('authors')
+  end
+  
+  describe 'author dir test' do
+    before(:all) do
+      Dir.mkdir(File.join(app.settings.public_dir, 'authors','biggers_john'))
+      Dir.mkdir(File.join(app.settings.public_dir, 'authors','nemerovski_bathsheba'))
+      testfile = File.open(File.join(app.settings.public_dir, 'authors' ,'testfile.txt'), 'w')
+      testfile.close
+      # app.set :authors, Proc.new { app.author_dir_load }
+    end
+    
+    after(:all) do
+      Dir.delete(File.join(app.settings.public_dir, 'authors','nemerovski_bathsheba'))
+      Dir.delete(File.join(app.settings.public_dir, 'authors','biggers_john'))
+      File.delete(File.join(app.settings.public_dir, 'authors' ,'testfile.txt'))
+    end
+    
+    it ":author_dir_load" do
+      expect(app.settings.authors).to include('biggers_john')
+    end
+    
+    it ":author_dir_load ordered" do
+      expect(app.settings.authors.first).to eq('biggers_john')
+    end
+    
+    it ":author_dir_load no files" do
+      expect(app.settings.authors).not_to include('testfile.txt')
+    end
+    
+    it ":author_name" do
+      expect(app.author_name(1)).to eq('Bathsheba Nemerovski')
+    end
+  end
+  
 end
