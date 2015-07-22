eba02497874bd2924895990dff60f1e5a80f4ed7
Frederick Wells <pandabrand@gmail.com>
2015-07-21 22:26:24 -0500

    removed bad variable call

--- comic_app.rb
+++ comic_app.rb
@@ -6,7 +6,7 @@ class ComicApp < Sinatra::Base
   set :static, true
   set :public_dir, File.dirname(__FILE__) + '/static'
   if !Dir.exist?(File.join(settings.public_dir, 'authors'))
-    Dir.mkdir(File.join(app.settings.public_dir, 'authors'))
+    Dir.mkdir(File.join(settings.public_dir, 'authors'))
   end
   set :authors, Proc.new { Dir.entries(File.join(settings.public_dir, 'authors')).select {|entry| File.directory? File.join(settings.public_dir, 'authors',entry) and !(entry =='.' || entry == '..') } }
 
@@ -14,20 +14,20 @@ class ComicApp < Sinatra::Base
     name_array = settings.authors[index].split('_')
     return name_array[1].capitalize + ' ' + name_array[0].capitalize
   end
-  
+
   get '/' do
     "John Biggers"
   end
-  
+
   get '/hi' do
     "Test"
   end
-  
+
   get '/:author' do
     author = params['author']
     redirect "/" + author + "/1"
   end
-  
+
   get '/:author/:page' do
     @author = params['author']
     @page = params['page']
